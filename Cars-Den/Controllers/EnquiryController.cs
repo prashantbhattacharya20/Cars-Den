3c16a59827682cdda58365bfed796c1c6bdf91b3
Prashant Bhattacharya <iprashantbhattacharya@gmail.com>
2024-05-31 15:08:21 +0530

    Completed

---
+++ Cars-Den/Controllers/EnquiryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cars_Den.Controllers
+{
+    public class EnquiryController : Controller
+    {
+        public IActionResult Enquiry()
+        {
+            return View();
+        }
+    }
+}
